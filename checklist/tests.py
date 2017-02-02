22942294bf8355afce329ef38972b025d6f00c5e
Arthur Vincent Simon <asimon@innovuze.com>
2017-02-02 13:14:49 +0800

    Partially implemented test

--- checklist/tests.py
+++ checklist/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class DummyTests(TestCase):
+    def test_dummy_pass(self):
+        self.assertIs(True, True)
+
+    def test_dummy_fail(self):
+        self.assertIs(True, False)
+
+class DummyViewTests(TestCase):
+    def test_home_page(self):
+        response = self.client.get('/')
+        print(response)
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_page(self):
+        response = self.client.get('admin/')
+        print(response)
+        self.assertEqual(response.status_code, 200)
